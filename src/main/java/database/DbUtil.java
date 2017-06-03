4217aece7e50a7b4f7110e466e91ba91125ff559
Kuldeep Singh <mca.kuldeep.sekhon@gmail.com>
2017-06-03 10:45:10 +0530

    dbutil connection

--- src/main/java/database/DbUtil.java
+++ src/main/java/database/DbUtil.java
@@ -1,5 +1,6 @@
 package database;
 
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -7,17 +8,13 @@ import java.sql.SQLException;
 public class DbUtil {
 	public static Connection getConnection(){
 		Connection conn=null;
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/db_matrimony","root","");
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return conn;
+		 String dbUrl = System.getenv("JDBC_DATABASE_URL");
+		    try {
+		    	conn= DriverManager.getConnection(dbUrl);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		    return conn;
 	}
 
 }
